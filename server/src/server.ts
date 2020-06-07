a6dda6fb8ce6eac0b5e99f06baa8f79c95569bdd
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2020-06-07 19:34:00 -0300

    base web

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (req, res) => {
-  res.send(['a', 'Ferreira', 'de', 'oasaliveira']);
+  res.send(['Arthur']);
 });
 
 app.listen(3333);
