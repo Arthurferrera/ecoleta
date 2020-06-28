2f55a942bb640fcbbca0bcfea97bc0180bd06c09
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2020-06-28 02:11:14 -0300

    all routes / cors - backend done

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,24 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'point_id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  }
+
   async create(req: Request, res: Response) {
     const {
       name,
@@ -39,11 +57,32 @@ class PointsController {
     })
   
     await trx('point_items').insert(pointItems);
+
+    await trx.commit();
   
     return res.json({
       id: point_id,
       ...point
     });
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return res.status(400).json({ message: 'Poin not found'});
+    }
+
+    const items = await knex('items')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
+      
+    const pointComplete = {...point, items};
+
+    return res.json(pointComplete);
   }
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import knex from './database/connection';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -9,12 +8,8 @@ const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
-
-// routes.get('/users/:id', (req, res) => {
-//   const id = +req.params.id;
-//   return res.json({message: 'ok'});
-// });
-
 routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
