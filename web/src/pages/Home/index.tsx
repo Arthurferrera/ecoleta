2811885d7080c2ef8a3cf934ba01eebb8c174867
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2020-07-03 00:37:15 -0300

    create mobile project

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -11,11 +11,19 @@ const Home: React.FC = () => {
       <div className="content">
         <header>
           <img src={logo} alt="Ecoleta"/>
-        </header>
+        </header> 
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
-
+          {/* aula 1 */}
+          {/* <iframe src="https://anchor.fm/rhavi-carneiro/embed/episodes/Walk-n-Talk-Especial-Fluency-Week-2-Ingls---Aula-01-eg4jf1/a-a2jf92k" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe> */}
+          
+          {/* aula 2 */}
+          {/* <iframe src="https://anchor.fm/fluency-week/embed/episodes/Ingls---Segunda-aula-prtica-da-Fluency-Week-2-eg4k9g/a-a2jfe13" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe> */}
+          {/* <iframe src="https://anchor.fm/rhavi-carneiro/embed/episodes/Walk-n-Talk-Especial-Fluency-Week-2-Ingls---Aula-02-eg4joh/a-a2jfao7" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe> */}
+          {/* aula 3 gavin */}
+          {/* <iframe src="https://anchor.fm/fluency-week/embed/episodes/Ingls---Terceira-aula-prtica-da-Fluency-Week-2-eg5bgo/a-a2jjo2p" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe> */}
+          {/* <iframe src="https://anchor.fm/rhavi-carneiro/embed/episodes/Walk-n-Talk-Especial-Fluency-Week-2-Ingls---Aula-03-eg5bjt/a-a2jv7uq" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe> */}
           <Link to="/create-point">
             <span>
               <FiLogIn />
