e588e3fd09ed87ba70ffe851ec5a12d0102a34eb
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2020-07-11 15:18:20 -0300

    data entry validation - backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
+import { errors } from 'celebrate';
 import routes from './routes';
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(3333);
